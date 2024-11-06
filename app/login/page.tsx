d55d39fb41276f98fba6a05fbcf5d76f4f309879
Srhiulli <srhiulli@gmail.com>
2024-11-06 08:54:37 -0300

    add tsconfig to jsx preserve

---
+++ app/login/page.tsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+
+const LoginPage = () => {
+  return (
+    <div className="gird grid-cols-2">
+      {/* DIREITA */}
+      <Image />
+    </div>
+  );
+};
+
+export default LoginPage;
