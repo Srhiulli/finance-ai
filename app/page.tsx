413ab9b70b546a7cda6ce79484f6efbb404082d2
Srhiulli <srhiulli@gmail.com>
2024-11-06 08:11:39 -0300

    add shadcn

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,8 @@
-
-export default function Home() {
+const Home = () => {
   return (
-    <h1>Home</h1>
+    <div>
+      <h1 className="flex p-5 text-red-500">Home</h1>
+    </div>
   );
-}
+};
+export default Home;
