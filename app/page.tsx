a39265aaf73301905ef953ce6ac50872505c31f2
Srhiulli <srhiulli@gmail.com>
2024-11-12 08:26:18 -0300

    add logout

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,15 @@
-const Home = () => {
-  return (
-    <div>
-      <h1 className="flex p-5 text-red-500">Home</h1>
+import { UserButton } from "@clerk/nextjs";
+import { auth } from "@clerk/nextjs/server";
+import { dark } from "@clerk/themes";
+import { redirect } from "next/navigation";
+
+const Home = async () => {
+  const {userId} = await auth ()
+  if (!userId){
+    redirect("/login")
+  }
+  return <div className="flex h-full items-center justify-center">
+    <UserButton showName/>
     </div>
-  );
 };
 export default Home;
