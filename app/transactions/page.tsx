e7e98b133ba459eab362c1cb49b692ede922657e
Srhiulli <srhiulli@gmail.com>
2024-11-17 16:16:10 -0300

    chore: add button add transaction

--- app/transactions/page.tsx
+++ app/transactions/page.tsx
@@ -1,7 +1,7 @@
 import { db } from "../_lib/prisma";
 import { DataTable } from "../_components/ui/data-table";
 import { transactionColumns } from "./_columns";
-// import AddTransactionButton from "../_components/add-transaction-button";
+import AddTransactionButton from "../_components/add-transaction-button";
 // import Navbar from "../_components/navbar";
 import { auth } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
@@ -22,7 +22,7 @@ const TransactionsPage = async () => {
         {/* TÍTULO E BOTÃO */}
         <div className="flex w-full items-center justify-between">
           <h1 className="text-2xl font-bold">Transações</h1>
-          {/* <AddTransactionButton userCanAddTransaction={userCanAddTransaction} /> */}
+          <AddTransactionButton />
         </div>
         {/* {/* <ScrollArea className="h-full"> */}
         <DataTable
